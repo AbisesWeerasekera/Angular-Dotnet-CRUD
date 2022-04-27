9e3834c33a9fc37809afc2991efd1efe7f16a1cc
AbisesWeerasekera <52200609+AbisesWeerasekera@users.noreply.github.com>
2022-04-27 13:59:41 +0530

    added mini form ui

--- src/app/models/register.model.ts
+++ src/app/models/register.model.ts
@@ -1,2 +1,8 @@
+//crate the model of USER table in MS SQL REGISTER database 
+//created by ng class models/register --type=model
 export class Register {
+  Id: number;
+  Name: string;
+  Age: number;
+  Address: string;
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { RegisterService } from '../service/register.service';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,27 @@ import { NgForm } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  //if we make it private , the component.html can't access it.
+  constructor(public _registerService:RegisterService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  //when the form is loading we want to set the form value to initial values
+  //we check the form object is null or not,then assign the input fileds to default values by using resetForm() funtion
+
+
+  resetForm(form?:NgForm){
+    if(form!=null){
+      this.resetForm();
+      this._registerService.formData={
+        Id:0,
+        Name:'',
+        Age:0,
+        Address:''
+      }
+    }
   }
 
 }
